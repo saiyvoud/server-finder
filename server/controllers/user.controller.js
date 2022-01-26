72df2b86716b4757eec38e93735138635e8eaeb1
thongmouaxiong <thongmouaxiong@gmail.com>
2022-01-26 23:31:00 +0700

    add

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -39,11 +39,25 @@ class UserController {
 
   static async signUp(req, res) {
 
-    let { firstname, lastname, username, email, password, phone } = req.body;
-    
+    let { firstname, lastname, username, password, phone } = req.body;
+    if (!firstname) {
+      return res.status(400).json({ msg: "firstname field is required." });
+    }
+    if (!lastname) {
+      return res.status(400).json({ msg: "lastname field is required." });
+    }
+    if (!username) {
+      return res.status(400).json({ msg: "username field is required." });
+    }
+    if (!password) {
+      return res.status(400).json({ msg: "password field is required." });
+    }
+    if (!phone) {
+      return res.status(400).json({ msg: "phone field is required." });
+    }
+
     // let phone = "+85620" + user_data.phone.substr(user_data.phone.length - 8, 8);
     try {
-      //   const userNew = new User({...user_data, phone});
       const userNew = new User({
         firstname,
         lastname,
@@ -85,8 +99,13 @@ class UserController {
 
   static async logIn(req, res) {
     try {
-      const { username, email, password } = req.body;
-
+      const { username, password } = req.body;
+      if (!username) {
+        return res.status(400).json({ msg: "username field is required." });
+      }
+      if (!password) {
+        return res.status(400).json({ msg: "password field is required." });
+      }
       let user = await User.findOne({ email });
       if (username) {
         user = await User.findOne({ username });
@@ -95,7 +114,7 @@ class UserController {
       if (!user)
         return res
           .status(404)
-          .json({ msg: "Invalidn username, email or password" });
+          .json({ msg: "Invalid username, email or password" });
 
       user.comparePassword(password, (err, isMatch) => {
         if (!isMatch)
