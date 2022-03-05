6058d9fb3909dfaf1ebe7ecc95a679483ea29ca0
thongmouaxiong <thongmouaxiong@gmail.com>
2022-03-06 00:10:23 +0700

    add

--- server/middlewares/shopkeeper.js
+++ server/middlewares/shopkeeper.js
@@ -1,7 +1,16 @@
+import mongoose from "mongoose";
+
 const shopkeeper = (req, res, next) => {
+  const obj_id = new mongoose.Types.ObjectId();
   if (req.user.auth === "admin" || req.user.auth === "shopkeeper")
     return next();
-  else return res.send("You are not allowed, get out now.");
+  else
+    return res
+      .status(400)
+      .json({
+        msg: "You are not allowed, get out now.",
+        shop: { _id: obj_id, isActive: false },
+      });
 };
 
 export { shopkeeper };
