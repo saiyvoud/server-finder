bf270c95d5175957ddb09706c55ad1bfa12d45d0
saiyvoud <76910592+saiyvoud@users.noreply.github.com>
2023-02-28 15:10:09 +0700

    add

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ import express from "express";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 import cors from 'cors'
-
 import dotenv from 'dotenv'
 dotenv.config()
 
